92b9ad8bc945a6c78d9750e7961d9c266c5bce0a
G9 Aerospace <g9militants@gmail.com>
2024-02-02 21:13:38 +0530

    🛠️ global.js -> Added functionality to display and remove loader while loading content. 🟢 history.html -> Added loader container. 🟢 index.html -> Added loader container.

--- public/assets/js/global.js
+++ public/assets/js/global.js
@@ -2,15 +2,52 @@
 const loadHTML = (filePath, containerId) => {
     const container = document.getElementById(containerId);
     if (container) {
+        // Display loader while loading
+        container.innerHTML = '<div id="loader-container"></div>';
+        const loaderContainer = document.getElementById("loader-container");
+        if (loaderContainer) {
+            loadLoader("assets/common/loader.html", "loader-container");
+        }
+
         fetch(filePath)
             .then(response => response.text())
             .then(data => {
+                // Remove loader once content is loaded
                 container.innerHTML = data;
+                stopLoader();
             })
             .catch(error => {
                 console.error(`Error loading HTML: ${error}`);
+                stopLoader();
             });
     }
+};
+
+// Function to load the loader
+const loadLoader = (filePath, containerId) => {
+    const loaderContainer = document.getElementById(containerId);
+    if (loaderContainer) {
+        fetch(filePath)
+            .then(response => response.text())
+            .then(data => {
+                // Insert loader content
+                loaderContainer.innerHTML = data;
+            })
+            .catch(error => {
+                console.error(`Error loading loader HTML: ${error}`);
+            });
+    }
+};
+
+// Function to stop the loader animation
+const stopLoader = () => {
+    const loaderContainer = document.getElementById("loader-container");
+    if (loaderContainer) {
+        loaderContainer.innerHTML = ''; // Remove loader content
+    }
+
+    // Additional checks or actions after the loader is stopped
+    console.log("Loader stopped. Perform additional checks or actions here.");
 };
 
 // Load header and footer
