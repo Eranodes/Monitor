5f257f9a99f28b3379f7be1bd3f323c4840b74f2
Dave <dave.turmawan@outlook.com>
2024-02-12 09:37:29 +0000

    Some stuff

--- serve.js
+++ serve.js
@@ -6,7 +6,7 @@ const packageJson = require('./package.json');
 
 const app = express();
 const port1 = process.env.PORT || 3000;
-const port2 = process.env.PORT_2 || 3001;
+const port2 = process.env.WEB_PORT || 3001;
 
 app.use(express.static(path.join(__dirname, 'public'), { extensions: ['html'] }));
 
@@ -29,7 +29,7 @@ app.listen(port1, () => {
   -----------------------
   Please read the license and readme.
   Ensure your .env file is properly filled.
-  Using ${port1} for serving status page and ${port2} for querying the eranodes sites!
+  Using ${port1} for serving status page and ${port2} for querying the EraNodes sites!
   For support, join our Discord server: https://discord.gg/jhju3spUbE
   -----------------------
   Server is running on port: ${port1}
